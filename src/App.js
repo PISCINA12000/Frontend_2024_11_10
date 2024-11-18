a68a264a6818fe1118b299c218c8fed6b5ae2555
= <=>
2024-11-18 10:05:43 -0300

    adicionando o redux no front

--- src/App.js
+++ src/App.js
@@ -6,35 +6,39 @@ import Tela404 from "./componentes/Telas/Tela404";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import TelaLogin from "./componentes/Telas/TelaLogin";
 import { useState, createContext } from 'react';
+import store from "./redux/store"
+import { Provider } from "react-redux"
 
 export const ContextoUsuario = createContext();
 function App() {
   const [usuario, setUsuario] = useState({
-    "usuario":"",
-    "logado":false
+    "usuario": "",
+    "logado": false
   });
-  if (!usuario.logado){
+  if (!usuario.logado) {
     return (
-      <ContextoUsuario.Provider value = {{ usuario, setUsuario }}>
-         <TelaLogin/>
+      <ContextoUsuario.Provider value={{ usuario, setUsuario }}>
+        <TelaLogin />
       </ContextoUsuario.Provider>
     );
   }
-  else{
+  else {
     return (
       <div className="App">
-        <ContextoUsuario.Provider value = {{ usuario, setUsuario }}>
-          <BrowserRouter>
-            { //A ordem das rotas é importante 
-            }
-            <Routes>
-              <Route path="/produto" element={<TelaCadastroProduto />} />
-              <Route path="/categoria" element={<TelaCadastroCategoria />} />
-              <Route path="/" element={<TelaMenu />} />
-              <Route path="*" element={<Tela404 />} />
-            </Routes>
-          </BrowserRouter>
-        </ContextoUsuario.Provider>
+        <Provider store={store}>
+          <ContextoUsuario.Provider value={{ usuario, setUsuario }}>
+            <BrowserRouter>
+              { //A ordem das rotas é importante 
+              }
+              <Routes>
+                <Route path="/produto" element={<TelaCadastroProduto />} />
+                <Route path="/categoria" element={<TelaCadastroCategoria />} />
+                <Route path="/" element={<TelaMenu />} />
+                <Route path="*" element={<Tela404 />} />
+              </Routes>
+            </BrowserRouter>
+          </ContextoUsuario.Provider>
+        </Provider>
       </div>
     );
   }
--- src/componentes/Telas/TelaCadastroProduto.jsx
+++ src/componentes/Telas/TelaCadastroProduto.jsx
@@ -4,11 +4,9 @@ import Pagina from "../layouts/Pagina";
 import { useEffect, useState } from "react";
 import TabelaProdutos from "./Tabelas/TabelaProdutos";
 //import { produtos } from "../../dados/mockProdutos";
-import { consultarProduto } from "../../servicos/servicoProduto";
 
 export default function TelaCadastroProduto(props) {
     const [exibirTabela, setExibirTabela] = useState(true);
-    const [listaDeProdutos, setListaDeProdutos] = useState([]);
     const [modoEdicao, setModoEdicao] = useState(false);
     //const [produtos, setProdutos] = useState([]);
     const [produtoSelecionado, setProdutoSelecionado] = useState({
@@ -27,6 +25,7 @@ export default function TelaCadastroProduto(props) {
             setListaDeProdutos(lista)
         })
     }, []) //listaVazia
+
     useEffect(() => {
         consultarProduto().then((lista) => {
             setListaDeProdutos(lista)
@@ -45,16 +44,12 @@ export default function TelaCadastroProduto(props) {
                 {
                     exibirTabela ?
                         <TabelaProdutos
-                            listaDeProdutos={listaDeProdutos}
-                            setListaDeProdutos={setListaDeProdutos}
                             setExibirTabela={setExibirTabela}
                             setModoEdicao={setModoEdicao}
                             setProdutoSelecionado={setProdutoSelecionado}
                         />
                         :
                         <FormCadProdutos
-                            listaDeProdutos={listaDeProdutos}
-                            setListaDeProdutos={setListaDeProdutos}
                             setExibirTabela={setExibirTabela}
                             produtoSelecionado={produtoSelecionado}
                             setProdutoSelecionado={setProdutoSelecionado}
